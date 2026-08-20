56ab49369c281ac00420302d6507474435f1a4b1
sverryboiii <sverreplaysgoodbs@gmail.com>
2026-08-20 20:17:26 +0200

    [min] Some small configuration settings are now changeable through __init__.py.

--- pytools/__init__.py
+++ pytools/__init__.py
@@ -18,15 +18,24 @@ def set_display(width: int, height: int, flags: int = 0, depth: int = 0, display
     Config.screen = pygame.display.set_mode((width, height), flags, depth, display, vsync)
     return Config.screen
 
-def max_fps(fps: int) -> None:
-    Config.max_fps = fps
+def max_rate(
+        fps: int | None = None,
+        tick: int | None = None
+) -> None:
+    """
+    :param fps: Caps the max frame rate (Can alwasy go below this number if anything is struggling).
+    :param tick: Sets the max tick rate (If the frame rate is lower than the tick rate the ticks will build up, so this is not a cap)
+    """
+    Config.max_fps = fps if fps else Config.max_fps
+    Config.tick_rate = tick if tick else Config.tick_rate
 
 def set_font(
         font: str = "arial",
         size: int = 25,
         bold: bool = False,
         italic: bool = False,
-        constructor: Optional[Callable[[Optional[str], int, bool, bool], pygame.font.Font]] = None):
+        constructor: Optional[Callable[[Optional[str], int, bool, bool], pygame.font.Font]] = None
+) -> pygame.font.Font:
     """
     :param font: The font of the text.
     :param size: How big the font is.
@@ -37,6 +46,30 @@ def set_font(
     """
     Config.font = pygame.font.SysFont(font, size, bold, italic, constructor)
     return Config.font
+
+def set_quit_method(method: Callable) -> None:
+    """
+    :param method: This method/function gets ran when the player presses Alt + F4 or the X in the top right.
+    """
+    Config.exit_function = method
+
+def set_frame_method(method: Callable) -> None:
+    """
+    :param method: This method/function gets ran every frame.
+    """
+    Config.frame_function = method
+
+def set_tick_method(method: Callable) -> None:
+    """
+    :param method: This method/function gets ran every tick.
+    """
+    Config.frame_function = method
+
+def get_delta() -> int | float:
+    """
+    :return: Returns the time it took for the last frame in ms.
+    """
+    return Config.delta_time
 
 def start():
     Runtime.start()
